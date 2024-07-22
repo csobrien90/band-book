9f95592e230b8210931a43a38cc402b04054ccf3
Chad OBrien <obrien.music@gmail.com>
2024-07-21 21:23:27 -0400

    fill out secondsToFormattedTime function

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -1,8 +1,17 @@
 /**
  * Returns a readable time string given a number of seconds
  * @param {number} seconds 
- * @returns {string} formattedTime
+ * @returns {string} formattedTime (HH:MM:SS)
  */
 export const secondsToFormattedTime = (seconds) => {
-    return ""
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return [
+        hours,
+        minutes,
+        remainingSeconds
+    ].map((unit) => String(unit).padStart(2, '0'))
+    .join(':');
 }
