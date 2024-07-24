237bed4f493b33d699a011130fbd90f28bbedf75
Chad OBrien <obrien.music@gmail.com>
2024-07-24 09:12:09 -0400

    implement indexedDB for local syncing

--- src/assets/classes/BandBook.js
+++ src/assets/classes/BandBook.js
@@ -6,14 +6,15 @@ import { Song } from './Song.js'
 export class BandBook {
 	/**
 	 * @constructor
-	 * @param {Array} songData - An array of song data objects
 	 * @param {HTMLElement} wrapperElement - An HTML element to render the workspace
 	 * @returns {BandBook} - A new BandBook instance
 	*/
-	constructor(songData = [], wrapperElement) {
-		if (songData.length === 0) songData = JSON.parse(localStorage.getItem('bandbook')) || []
-		this.songData = songData
+	constructor(wrapperElement) {
 		this.wrapper = wrapperElement
+		this.navElement = null
+		this.songData = []
+		this.load()
+		
 		this.init()
 	}
 
@@ -21,7 +22,7 @@ export class BandBook {
 	 * Initializes the BandBook instance
 	 * @returns {void}
 	*/
-    init() {
+    init() {		
 		// Create an array of Song instances from the song data
         this.songs = this.songData.map(song => {
 			return new Song(song, () => this.sync())
@@ -55,6 +56,7 @@ export class BandBook {
 	 * @returns {void}
 	*/
 	renderSongNavigation() {
+		if (this.navElement) this.navElement.remove()
 		const navigation = document.createElement('nav')
 		navigation.classList.add('song-navigation')
 
@@ -64,6 +66,10 @@ export class BandBook {
 			button.addEventListener('click', () => this.setWorkspace(song))
 			navigation.appendChild(button)
 		})
+
+		navigation.appendChild(this.getAddSongButton())
+
+		this.navElement = navigation
 
 		this.wrapper.parentElement.insertBefore(navigation, this.wrapper)
 	}
@@ -79,22 +85,167 @@ export class BandBook {
 	}
 
 	/**
-	 * Sync the BandBook instance with localStorage
+	 * Sync the BandBook instance with indexedDB
 	 */
 	sync() {
 		if (!this.songs || !this.songs.length) return
 		const data = this.songs.map(song => song.getData())
-		localStorage.setItem('bandbook', JSON.stringify(data))
+		const request = indexedDB.open('bandbook', 1)
+
+		request.onupgradeneeded = (e) => {
+			const db = e.target.result
+			const store = db.createObjectStore('books', { keyPath: 'id' })
+			store.createIndex('id', 'id', { unique: true })
+		}
+
+		request.onerror = (e) => {
+			console.error('Error opening indexedDB', e)
+		}
+
+		request.onsuccess = (e) => {
+			const db = e.target.result
+			const transaction = db.transaction(['books'], 'readwrite')
+			const store = transaction.objectStore('books')
+
+			// If the data is empty, delete the record
+			if (!data.length) {
+				const request = store.delete(this.id)
+				request.onsuccess = () => {
+					// console.log('Data deleted successfully')
+				}
+				request.onerror = (e) => {
+					console.error('Error deleting data', e)
+				}
+
+				return
+			}
+
+			if (this.id) {
+				// Otherwise, update the record
+				const existing = store.get(this.id)
+				existing.onsuccess = () => {
+					const record = existing.result
+					if (record) {
+						record.data = JSON.stringify(data)
+						store.put(record)
+					} else {
+						store.add({ id: this.id, data: JSON.stringify(data) })
+					}
+				}
+			} else {
+				// If there is no ID, make one
+				if (!this.id) this.id = this.createId
+				store.add({ id: this.id, data: JSON.stringify(data) })
+			}
+
+		}
 	}
 
 	/**
-	 * Load the BandBook instance from localStorage
+	 * Load the BandBook instance from indexedDB
 	 */
 	load() {
-		const data = JSON.parse(localStorage.getItem('bandbook'))
-		if (data) {
-			this.songData = data
-			this.init()
+		const request = indexedDB.open('bandbook', 1)
+
+		request.onupgradeneeded = (e) => {
+			const db = e.target.result
+			const store = db.createObjectStore('books', { keyPath: 'id' })
+			store.createIndex('id', 'id', { unique: true })
 		}
+
+		request.onerror = (e) => {
+			console.error('Error opening indexedDB', e)
+		}
+
+		request.onsuccess = async (e) => {
+			const db = e.target.result
+			const transaction = db.transaction(['books'], 'readwrite')
+			const store = transaction.objectStore('books')
+
+
+			if (this?.id) {
+				const existing = store.get(this.id)
+				existing.onsuccess = (e) => {
+					const record = e.target.result
+					if (record) {
+						const data = JSON.parse(record.data)
+						if (data) this.songData = data
+						this.init()
+					}
+				}
+			} else {
+				const all = store.getAll()
+	
+				all.onsuccess = (e) => {
+					try {
+						const data = JSON.parse(e.target.result[0]?.data)
+						if (data) {
+							this.songData = data
+							this.id = e.target.result[0]?.id
+						} else {
+							// If there is no ID, make one
+							if (!this.id) this.id = this.createId
+						}
+						this.init()
+					} catch (e) {
+						console.error('Error parsing data', e)
+					}
+				}
+				
+				all.onerror = (e) => {
+					console.error('Error loading data', e)
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the add song button
+	 * @returns {HTMLButtonElement} - A button element
+	*/
+	getAddSongButton() {
+		const upload = document.createElement('input')
+		upload.type = 'file'
+		upload.accept = 'audio/*'
+		upload.classList.add('btn')
+		upload.addEventListener('change', (e) => {
+			// Validate the file type
+			const fileType = e.target.files[0].type
+			if (!fileType.includes('audio')) {
+				// Alert the user if the file type is invalid
+				alert('Please upload a valid file type (e.g. mp3, wav)')
+
+				// Reset the input and return
+				upload.value = ''
+				return
+			}
+
+			// base64 encode the file
+			const reader = new FileReader()
+			reader.onload = (readerEvent) => {
+				const base64 = readerEvent.target.result
+				const songData = {
+					src: base64,
+					title: e.target.files[0].name,
+					composer: 'Unknown'
+				}
+
+				const song = new Song(songData, () => this.sync())
+				this.addSong(song)
+				this.renderSongNavigation()
+				this.sync()
+			}
+			reader.readAsDataURL(e.target.files[0])
+		})
+
+		return upload
+	}
+
+	/**
+	 * Creates a unique ID
+	 * @returns {string} - A unique ID
+	*/
+	get createId() {
+		return crypto.randomUUID()
 	}
 }
